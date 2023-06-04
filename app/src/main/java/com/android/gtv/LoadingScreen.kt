13987ba7a082c88705ac7f854fe8db026dbaffe2
DESKTOP-OC64MCJ\CARLOS <carlos.athsnasius@gmail.com>
2023-06-04 22:36:15 +0700

    Done 50%

---
+++ app/src/main/java/com/android/gtv/LoadingScreen.kt
@@ -0,0 +1,30 @@
+package com.android.gtv
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowManager
+import androidx.fragment.app.FragmentActivity
+
+class LoadingScreen : FragmentActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_loading_screen)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAGS_CHANGED
+        )
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 1000)
+
+    }
+}
---
+++ app/src/main/java/com/android/gtv/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.android.gtv
+
+import android.content.Intent
+import android.graphics.Color.alpha
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.fragment.app.FragmentActivity
+
+class MainActivity : FragmentActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
